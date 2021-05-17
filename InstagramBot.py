f9b6078bb6e46de057a72a4b8f6366759e841ac8
Flavio Caetano <flavioluzio22@gmail.com>
2021-05-17 12:35:09 -0300

    atualizando

--- InstagramBot.py
+++ InstagramBot.py
@@ -5,13 +5,15 @@ class InstagramBot:
     def __init__(self, username, password):
         self.username = username
         self.password = password
-        self.driver = webdriver.Chrome()
+        self.driver = webdriver.Chrome(executable_path='./chromedriver.exe')
 
     def openInstagram(self):
+        print("Opening instagram...")
         self.driver.get("https://instagram.com")
         sleep(1)
 
     def logIn(self):
+        print("Login...")
         self.driver.find_element_by_xpath("//input[@name=\"username\"]")\
             .send_keys(self.username)
         self.driver.find_element_by_xpath("//input[@name=\"password\"]")\
@@ -21,6 +23,7 @@ class InstagramBot:
         sleep(5)
 
     def logOut(self):
+        print("Logout...")
         self.driver.find_element_by_xpath("//*[@id=\"react-root\"]/section/nav/div[2]/div/div/div[3]/div/div[5]/div[1]")\
             .click()
         self.driver.find_element_by_xpath("//*[@id=\"f16163356daea18\"]")\
@@ -28,17 +31,21 @@ class InstagramBot:
         sleep(1)
 
     def close(self):
+        print("Closing...")
         self.driver.quit()
 
     def closeModals(self):
+        print("Closing modal 1...")
         self.driver.find_element_by_xpath("//button[contains(text(), 'Not Now')]")\
             .click()
         sleep(2)
+        print("Closing modal 2...")
         self.driver.find_element_by_xpath("//button[contains(text(), 'Not Now')]")\
             .click()
         sleep(2)
 
     def acessProfile(self):
+        print("Accessing profile...")
         self.driver.get("https://instagram.com/" + self.username)
         sleep(2)
 
@@ -47,12 +54,20 @@ class InstagramBot:
         followersList = self._getFollowersList()
 
         notFollowingUsers = [user for user in followingList if user not in followersList]
-        notFollowingFile = open('notFollowing.txt', 'w')
-        for names in notFollowingUsers:
-            notFollowingFile.write(names + "\n")
-        notFollowingFile.close()
+
+        self._saveList(notFollowingUsers, 'notFollowing', 'notFollowing.txt')
+        self._saveList(followingList, 'following', 'following.txt')
+        self._saveList(followersList, 'followers', 'followers.txt')
+
+    def _saveList(self, content, type, fileName):
+        print("Saving " + type + " list...")
+        fileItem = open(fileName, 'w')
+        for item in content:
+            fileItem.write(item + "\n")
+        fileItem.close()
 
     def _getFollowersList(self):
+        print("Getting followers list...")
         self.driver.find_element_by_xpath("//a[contains(@href, '/followers')]")\
             .click()
         sleep(1)
@@ -69,10 +84,6 @@ class InstagramBot:
 
         links = scrollBox.find_elements_by_tag_name("a")
         names = [name.text for name in links if name.text != '']
-        followersFile = open('followers.txt', 'w')
-        for name in names:
-            followersFile.write(name + "\n")
-        followersFile.close()
 
         self.driver.find_element_by_xpath("/html/body/div[5]/div/div/div[1]/div/div[2]/button")\
             .click()
@@ -80,6 +91,7 @@ class InstagramBot:
         return names
 
     def _getFollowingList(self):
+        print("Getting following list...")
         self.driver.find_element_by_xpath("//a[contains(@href, '/following')]")\
             .click()
         sleep(1)
@@ -96,10 +108,6 @@ class InstagramBot:
 
         links = scrollBox.find_elements_by_tag_name("a")
         names = [name.text for name in links if name.text != '']
-        followingFile = open('following.txt', 'w')
-        for name in names:
-            followingFile.write(name + "\n")
-        followingFile.close()
 
         self.driver.find_element_by_xpath("/html/body/div[5]/div/div/div[1]/div/div[2]/button")\
             .click()
---
+++ README.md
@@ -0,0 +1,34 @@
+# Instagram Bot
+
+## Descrição do projeto
+Bot construído com objetivos de estudo sobre automatização de ações em navegadores
+
+## Configurações:
+
+1. Crie um arquivo chamado "secrets.py" e nele escreva a seguinte estrutura:
+
+    ```
+        username="seu_usuario_do_instagram"
+        password="sua_senha_do_instagram"
+    ```
+
+    - Dessa forma, seus dados não ficam visíveis para mais nada além da aplicação.
+
+    **Observação**: É imprescindível que a extensão do arquivo seja **.py**, para o funcionamento correto.
+
+1. Download do driver para o navegador automatizado
+
+    - Para o funcionamento correto, acesse o link do [Chromedriver](https://chromedriver.chromium.org/downloads) e baixe o arquivo correspondente a sua versão.
+
+1. Coloque o arquivo **.exe** na mesma pasta do projeto
+
+
+## Tecnologia:
+
+A seguinte ferramenta foi utilizada:
+
+- [Python 3](https://www.python.org/download/releases/3.0/)
+
+https://chromedriver.chromium.org/downloads
+
+![Badge](https://img.shields.io/pypi/l/selenium)
